7bb5dcff6231b401cbe3e9985477c8696d76cf6c
Anjali Brahmasandra <anjalib2325@gmail.com>
2022-08-04 17:15:10 -0400

    fixed all buttons and finished the app

--- herstoryLibraryApp/topic1ResourcesViewController.swift
+++ herstoryLibraryApp/topic1ResourcesViewController.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 class topic1ResourcesViewController: UIViewController {
-
+    @IBOutlet var equalpayButton: UIButton!
+    @IBOutlet var paydayButton: UIButton!
+    @IBOutlet var rightsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func equalpayButton(_ sender: UIButton) {
+        let url = URL (string: "http://www.equalpaytoday.org/")!
+        UIApplication.shared.open (url)
     }
-    */
-
+    @IBAction func paydayButton(_ sender: UIButton) {
+        let url2 = URL (string: "https://www.npr.org/2022/03/16/1086732450/on-equal-pay-day-women-are-trying-to-make-a-dollar-out-of-83-cents")!
+        UIApplication.shared.open (url2)
+    }
+    
+    @IBAction func rightsButton(_ sender: UIButton) {
+        let url3 = URL (string: "https://www.equalrights.org/issue/economic-workplace-equality/pay-discrimination/")!
+        UIApplication.shared.open (url3)
+    }
+    
 }
--- herstoryLibraryApp/topic2ResourcesViewController.swift
+++ herstoryLibraryApp/topic2ResourcesViewController.swift
@@ -8,22 +8,29 @@
 import UIKit
 
 class topic2ResourcesViewController: UIViewController {
-
+    @IBOutlet var donateButton: UIButton!
+    @IBOutlet var infoButton: UIButton!
+    @IBOutlet var protestButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func donateButton(_ sender: UIButton) {
+        let url = URL (string: "https://www.womenshealthmag.com/life/a40407068/abortion-funds-donate/")!
+        UIApplication.shared.open (url)
     }
-    */
+    @IBAction func infoButton(_ sender: UIButton) {
+        let url2 = URL (string: "https://reproductiverights.org/maps/what-if-roe-fell/")!
+        UIApplication.shared.open (url2)
+    }
+    
+    @IBAction func protestButton(_ sender: UIButton) {
+        let url3 = URL (string: "https://actionnetwork.org/event_campaigns/day-of-day-after-response-to-supreme-court-decision-on-roe-v-wade?source=direct_link&&can_id=f4a0c88b61ec2db42439c1cb4d625acb&mobile_message_referrer=mobile-message_16657")!
+        UIApplication.shared.open (url3)
+    }
+    
 
 }
